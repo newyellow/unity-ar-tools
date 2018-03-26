dbcce40ffa02ce72521f20c750a95a97fa6f35f6
newyellow <newyellow2@gmail.com>
2018-03-26 14:33:11 +0800

    add a dynamic shadow distance script to deal with shadow resolution problem

---
+++ ARLightToolPack/Scripts/DynamicShadowDistance.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicShadowDistance : MonoBehaviour {
+
+	public Transform targetObj;
+	public float camToObjDist = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+		camToObjDist = Vector3.Distance (transform.position, targetObj.position);
+		QualitySettings.shadowDistance = camToObjDist * 1.5f;
+	}
+}
